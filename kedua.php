a451f5adeeb1e122208a669e4cea4ad2ea523689
mfazbi <47375721+mfazbi@users.noreply.github.com>
2020-10-21 09:39:45 +0700

    Add files via upload

---
+++ kedua.php
@@ -0,0 +1,8 @@
+<?php
+	for ($i=1; $i <= 10; $i++) { 
+		echo "<br/> Hallo, Namaku Rizqi Auliya Azbi";
+		echo "<br/> Saya Mahasiswa UNIGA, Prodi Sistem Informasi";
+		echo "<br/> Asal Jombang";
+		echo "<br/>";
+	}
+?>
---
+++ pertama.php
@@ -0,0 +1,15 @@
+<?php
+$nilai = "95";
+if ($nilai > "86") {
+	echo "selamat... proses belajarnyamu sangat baik, tingkatkan terus kemempuan codingnya...";
+}
+elseif ($nilai > "71") {
+	echo "selamat... kamu sudah mulai memahami pemrograman PHP, tingkatkan terus skill codingmu...";
+}
+elseif ($nilai > "56") {
+	echo "upsss... skill codingmu perlu diasah nih gaes, tetap semangat belajar dan exsplore kemampuan ya...";
+}
+else {
+	echo "Tingkatkan belajarnya ya gaes...";
+}
+?>
